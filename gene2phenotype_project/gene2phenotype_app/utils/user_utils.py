fa3e8b4031219c542173f9388f761ebbd0f3197d
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2025-01-14 11:22:43 +0000

    Password stuff

--- gene2phenotype_project/gene2phenotype_app/utils/user_utils.py
+++ gene2phenotype_project/gene2phenotype_app/utils/user_utils.py
@@ -23,3 +23,45 @@ class CustomMail():
             return response.status_code
         except Exception as e:
             return str(e)
+        
+
+    @staticmethod
+    def send_create_email(data, subject, to_email):
+        email_body = render_to_string('gene2phenotype_app/create_user_email.tpl', {
+            'user': data.first_name,
+            'email': data.email,
+            'first_name': data.first_name, 
+            'last_name': data.last_name,
+            'username': data.username
+        })
+        message = Mail(
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to_emails=to_email,
+            subject=subject,
+            html_content=email_body
+            )
+        try: 
+            sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+            response = sg.send(message)
+            return response.status_code
+        except Exception as e:
+            return str(e)
+        
+    @staticmethod
+    def send_change_password_email(user, user_email, subject, to_email):
+        email_body = render_to_string('gene2phenotype_app/change_password_email.tpl', {
+            'user': user,
+            'email': user_email
+        })
+        message = Mail(
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to_emails=to_email,
+            subject=subject,
+            html_content=email_body
+            )
+        try:
+            sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+            response = sg.send(message)
+            return response.status_code
+        except Exception as e:
+            return str(e)
