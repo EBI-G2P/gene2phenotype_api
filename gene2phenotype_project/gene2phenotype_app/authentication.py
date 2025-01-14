8c1bf7bbdb45390fc8be805c0dc80745739d54ba
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2025-01-14 11:41:56 +0000

    Authentication changes

--- gene2phenotype_project/gene2phenotype_app/authentication.py
+++ gene2phenotype_project/gene2phenotype_app/authentication.py
@@ -5,6 +5,8 @@ from rest_framework import HTTP_HEADER_ENCODING, authentication
 from django.middleware.csrf import CsrfViewMiddleware
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
     
 
 class CustomAuthentication(JWTAuthentication):
@@ -14,6 +16,10 @@ class CustomAuthentication(JWTAuthentication):
         
         if header is None:
             # getting authentication details from cookies
+            refresh_token = request.COOKIES.get(settings.SIMPLE_JWT['REFRESH_COOKIE'])
+            if refresh_token:
+                if self.is_token_blacklisted(refresh_token):
+                    raise AuthenticationFailed("Token has been blacklisted")
             raw_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE'])
         else:
             #just giving the option from headers but no longer being implemented
@@ -28,3 +34,13 @@ class CustomAuthentication(JWTAuthentication):
             raise AuthenticationFailed(str(e))
         return self.get_user(validated_token), validated_token
     
+    
+    def is_token_blacklisted(self, token_string):
+        try:
+            token = RefreshToken(token_string)
+            if BlacklistedToken.objects.filter(token__jti=token['jti']).exists():
+                return True
+        except Exception as e:
+            raise AuthenticationFailed(f"Token blacklist check failed: {e}")
+
+        return False
