52d8bec22a6baf936d30d2e8db08b52d2380e249
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2025-01-21 13:19:41 +0000

    Refresh time added to the profile

--- gene2phenotype_project/gene2phenotype_app/views/user.py
+++ gene2phenotype_project/gene2phenotype_app/views/user.py
@@ -284,9 +284,8 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
         """Retrieve and return authenticated user"""
         serializer = UserSerializer(request.user)
         refresh_token_lifetime = getattr(settings, "SIMPLE_JWT", {}).get("REFRESH_TOKEN_LIFETIME", timedelta(days=1))
-        refresh_token_time = datetime.utcnow() + refresh_token_lifetime
         result = serializer.data
-        result['refresh_token_time']  = refresh_token_time
+        result['refresh_token_time']  = refresh_token_lifetime
         return Response(result, status=status.HTTP_200_OK)
 
 class ChangePasswordView(generics.GenericAPIView):
