04c8a2a4c97044438c0848fc3d9f2c5b6f2f0e67
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2025-02-18 15:21:03 +0000

    Meta serializer info

--- gene2phenotype_project/gene2phenotype_app/serializers/meta.py
+++ gene2phenotype_project/gene2phenotype_app/serializers/meta.py
@@ -2,15 +2,39 @@ from rest_framework import serializers
 
 from ..models import Meta
 
+
 class MetaSerializer(serializers.ModelSerializer):
+    """
+    Meta Serializer for the model Meta
+    Args:
+        serializers (_type_): ModelSerializer
+                        Fields - Key, version, source
+
+    Returns:
+        _type_: Serializer fields
+    """
+
     key = serializers.CharField()
-    version = serializers.CharField() 
+    version = serializers.CharField()
     source = serializers.SerializerMethodField()
 
     def get_source(self, obj):
+        """
+        Used to get the source because the source is a ForeignKey in this table
+
+        Args:
+            obj (_type_): Queryset object
+
+        Returns:
+            _type_: source
+        """
         source = obj.source.name
         return source
 
     class Meta:
+        """
+        Meta class for this serializer
+        """
+
         model = Meta
         fields = ["key", "source", "version"]
