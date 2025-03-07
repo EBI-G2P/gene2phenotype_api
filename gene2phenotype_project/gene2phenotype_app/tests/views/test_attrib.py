7d70d549ec2d9486c90c52e2f56226092efd05bf
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2025-03-07 09:44:09 +0000

    Adding the final tests

--- gene2phenotype_project/gene2phenotype_app/tests/views/test_attrib.py
+++ gene2phenotype_project/gene2phenotype_app/tests/views/test_attrib.py
@@ -5,23 +5,29 @@ from django.urls import reverse
 from gene2phenotype_app.models import Attrib, AttribType
 
 class AttribTypeListTestEndpoint(TestCase):
+    fixtures = ["gene2phenotype_app/fixtures/attribs.json"]
     def setUp(self):
         self.url_attribtypelist = reverse("list_attrib_type")
 
     
     def test_attrib_type_list(self):
         response = self.client.get(self.url_attribtypelist)
+
         self.assertEqual(response.status_code, 200)
+        self.assertIn("confidence_category", response.data)
 
 
 class AttribListTestEndpoint(TestCase):
+    fixtures = ["gene2phenotype_app/fixtures/attribs.json"]
     def setUp(self):
-        self.attrib_code = "confidence_category/"
-        self.url_attriblist = reverse("list_attribs_by_type", args={'code': self.attrib_code})
+
+        self.url_attriblist = reverse("list_attribs_by_type", kwargs={"code": "confidence_category"})
         
     
     def test_attrib_list_code(self):
         response = self.client.get(self.url_attriblist)
 
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['count'], 6)
+        self.assertEqual(response.data["count"], 6)
+        
