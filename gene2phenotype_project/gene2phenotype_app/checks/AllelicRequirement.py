515b9fe973bd799b9f7095b64ada1f94a61db0d4
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2025-03-11 15:27:21 +0000

    Adding the changes for the allelic requirement DC

--- gene2phenotype_project/gene2phenotype_app/checks/AllelicRequirement.py
+++ gene2phenotype_project/gene2phenotype_app/checks/AllelicRequirement.py
@@ -31,11 +31,28 @@ class AllelicRequirementCheck:
             if "par" in obj.genotype.value.lower() and (obj.locus.sequence.name != "X" or obj.locus.sequence.name != "Y"):
                 errors.append(
                     Error(
-                        f"PAR genotype in not X or Y chromosome {obj.stable_id.stable_id}",
+                        f"PAR genotype not X or Y chromosome {obj.stable_id.stable_id}",
                         hint="Genotype PAR not in chromsome X or Y",
                         id="gene2phenotype_app.E004",
                     )
                 )
             
+            if "X" in obj.genotype.value and obj.locus.sequence.name != "X":
+                errors.append(
+                    Error(
+                        f"X genotype in a non X chromosome {obj.stable_id.stable_id}",
+                        hint="Genotype X linked not in chromosome X",
+                        id="gene2phenotype_app.E005",
+                    )
+                )
+            
+            if "Y" in obj.genotype.value and obj.locus.sequence.name != "Y":
+                errors.append(
+                    Error(
+                        f"Y genotype in a non Y chromsome {obj.stable_id.stable_id}",
+                        hint="Genotype Y linked not in chromsome Y",
+                        id="gene2phenotype_app.E006",
+                    )
+                )
         
         return errors
