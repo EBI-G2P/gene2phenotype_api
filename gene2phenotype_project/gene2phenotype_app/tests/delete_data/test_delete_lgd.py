d8ee71cbf06d1842b1b53e5e84b3737db8071757
dglemos <dlemos@ebi.ac.uk>
2025-03-20 13:03:03 +0000

    Finish tests for delete record

--- gene2phenotype_project/gene2phenotype_app/tests/delete_data/test_delete_lgd.py
+++ gene2phenotype_project/gene2phenotype_app/tests/delete_data/test_delete_lgd.py
@@ -6,23 +6,50 @@ from gene2phenotype_app.models import (
     User,
     G2PStableID,
     LocusGenotypeDisease,
-    LGDPanel
+    LGDPanel,
+    LGDComment,
+    LGDMolecularMechanismEvidence,
+    LGDMolecularMechanismSynopsis,
+    LGDPhenotype,
+    LGDPhenotypeSummary,
+    LGDVariantType,
+    LGDVariantTypeComment,
+    LGDVariantTypeDescription,
+    LGDVariantGenccConsequence,
+    LGDCrossCuttingModifier
 )
 
 class LGDDeleteLGDEndpoint(TestCase):
     """
         Test endpoint to delete a record (LGD)
     """
-    fixtures = ["gene2phenotype_app/fixtures/attribs.json", "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
-                "gene2phenotype_app/fixtures/disease.json", "gene2phenotype_app/fixtures/g2p_stable_id.json",
-                "gene2phenotype_app/fixtures/g2p_stable_id.json", "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
-                "gene2phenotype_app/fixtures/lgd_mechanism_evidence.json", "gene2phenotype_app/fixtures/lgd_mechanism_synopsis.json",
-                "gene2phenotype_app/fixtures/lgd_panel.json", "gene2phenotype_app/fixtures/locus_genotype_disease.json",
-                "gene2phenotype_app/fixtures/locus.json", "gene2phenotype_app/fixtures/publication.json",
-                "gene2phenotype_app/fixtures/sequence.json", "gene2phenotype_app/fixtures/user_panels.json",
-                "gene2phenotype_app/fixtures/ontology_term.json", "gene2phenotype_app/fixtures/source.json",
-                "gene2phenotype_app/fixtures/lgd_publication.json"
-                ]
+    fixtures = [
+        "gene2phenotype_app/fixtures/attribs.json",
+        "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
+        "gene2phenotype_app/fixtures/disease.json",
+        "gene2phenotype_app/fixtures/g2p_stable_id.json",
+        "gene2phenotype_app/fixtures/g2p_stable_id.json",
+        "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
+        "gene2phenotype_app/fixtures/lgd_mechanism_evidence.json",
+        "gene2phenotype_app/fixtures/lgd_mechanism_synopsis.json",
+        "gene2phenotype_app/fixtures/lgd_panel.json",
+        "gene2phenotype_app/fixtures/locus_genotype_disease.json",
+        "gene2phenotype_app/fixtures/locus.json",
+        "gene2phenotype_app/fixtures/publication.json",
+        "gene2phenotype_app/fixtures/sequence.json",
+        "gene2phenotype_app/fixtures/user_panels.json",
+        "gene2phenotype_app/fixtures/ontology_term.json",
+        "gene2phenotype_app/fixtures/source.json",
+        "gene2phenotype_app/fixtures/lgd_publication.json",
+        "gene2phenotype_app/fixtures/lgd_comment.json",
+        "gene2phenotype_app/fixtures/lgd_phenotype.json",
+        "gene2phenotype_app/fixtures/lgd_phenotype_summary.json",
+        "gene2phenotype_app/fixtures/lgd_variant_type.json",
+        "gene2phenotype_app/fixtures/lgd_variant_type_comment.json",
+        "gene2phenotype_app/fixtures/lgd_variant_type_description.json",
+        "gene2phenotype_app/fixtures/lgd_variant_consequence.json",
+        "gene2phenotype_app/fixtures/lgd_cross_cutting_modifier.json"
+    ]
 
     def setUp(self):
         self.url_delete_lgd = reverse("lgd_delete", kwargs={"stable_id": "G2P00002"})
@@ -83,3 +110,31 @@ class LGDDeleteLGDEndpoint(TestCase):
         self.assertEqual(lgd_obj.is_deleted, 1)
         lgd_panel_obj = LGDPanel.objects.get(lgd=lgd_obj.id)
         self.assertEqual(lgd_panel_obj.is_deleted, 1)
+        lgd_comment_obj = LGDComment.objects.get(lgd=lgd_obj.id)
+        self.assertEqual(lgd_comment_obj.is_deleted, 1)
+        mechanism_evidence_obj = LGDMolecularMechanismEvidence.objects.get(lgd=lgd_obj.id)
+        self.assertEqual(mechanism_evidence_obj.is_deleted, 1)
+        mechanism_synopsys_list = LGDMolecularMechanismSynopsis.objects.filter(lgd=lgd_obj.id)
+        for mechanism_synopsys in mechanism_synopsys_list:
+            self.assertEqual(mechanism_synopsys.is_deleted, 1)
+        phenotype_list = LGDPhenotype.objects.filter(lgd=lgd_obj.id)
+        for phenotype in phenotype_list:
+            self.assertEqual(phenotype.is_deleted, 1)
+        phenotype_summary_list = LGDPhenotypeSummary.objects.filter(lgd=lgd_obj.id)
+        for pheno_summary in phenotype_summary_list:
+            self.assertEqual(pheno_summary.is_deleted, 1)
+        lgd_variant_type_list = LGDVariantType.objects.filter(lgd=lgd_obj.id)
+        for variant_type in lgd_variant_type_list:
+            self.assertEqual(variant_type.is_deleted, 1)
+            variant_type_comment_list = LGDVariantTypeComment.objects.filter(lgd_variant_type=variant_type.id)
+            for variant_type_comment in variant_type_comment_list:
+                self.assertEqual(variant_type_comment.is_deleted, 1)
+        lgd_variant_type_desc_list = LGDVariantTypeDescription.objects.filter(lgd=lgd_obj.id)
+        for variant_type_desc in lgd_variant_type_desc_list:
+            self.assertEqual(variant_type_desc.is_deleted, 1)
+        lgd_variant_consequence_list = LGDVariantGenccConsequence.objects.filter(lgd=lgd_obj.id)
+        for variant_consequence in lgd_variant_consequence_list:
+            self.assertEqual(variant_consequence.is_deleted, 1)
+        lgd_ccm_list = LGDCrossCuttingModifier.objects.filter(lgd=lgd_obj.id)
+        for lgd_ccm in lgd_ccm_list:
+            self.assertEqual(lgd_ccm.is_deleted, 1)
