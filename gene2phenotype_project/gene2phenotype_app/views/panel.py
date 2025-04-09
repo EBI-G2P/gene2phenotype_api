745f81ce558a0e2c33d6cd0de9eeb94ff389162b
dglemos <dlemos@ebi.ac.uk>
2025-04-09 13:45:38 +0100

    Return 401 when no permission to access non visible panel

--- gene2phenotype_project/gene2phenotype_app/views/panel.py
+++ gene2phenotype_project/gene2phenotype_app/views/panel.py
@@ -88,6 +88,8 @@ class PanelDetail(BaseView):
         for panel in queryset:
             if panel.is_visible == 1 or (user.is_authenticated and panel.is_visible == 0):
                 flag = 1
+            elif panel.is_visible == 0 and not user.is_authenticated:
+                return self.handle_no_permission_authentication('Panel', name)
 
         if flag == 1:
             serializer = PanelDetailSerializer()
