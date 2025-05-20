3de1351781e4083e82face2549974e10327471f0
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2025-05-20 21:02:45 +0530

    format files

--- gene2phenotype_project/gene2phenotype_app/tests/curation/test_get_curation.py
+++ gene2phenotype_project/gene2phenotype_app/tests/curation/test_get_curation.py
@@ -4,18 +4,26 @@ from django.conf import settings
 from rest_framework_simplejwt.tokens import RefreshToken
 from gene2phenotype_app.models import User
 
+
 class LGDGetCurationDraftEndpoint(TestCase):
     """
-        Test endpoint to get curation draft
+    Test endpoint to get curation draft
     """
-    fixtures = ["gene2phenotype_app/fixtures/g2p_stable_id.json", "gene2phenotype_app/fixtures/user_panels.json", "gene2phenotype_app/fixtures/curation_data.json"]
+
+    fixtures = [
+        "gene2phenotype_app/fixtures/g2p_stable_id.json",
+        "gene2phenotype_app/fixtures/user_panels.json",
+        "gene2phenotype_app/fixtures/curation_data.json",
+    ]
 
     def setUp(self):
-        self.url_get_curation = reverse("curation_details", kwargs={"stable_id": "G2P00004"})
+        self.url_get_curation = reverse(
+            "curation_details", kwargs={"stable_id": "G2P00004"}
+        )
 
     def test_get_curation_success(self):
         """
-            Test successful call to get curation draft endpoint
+        Test successful call to get curation draft endpoint
         """
         # Login
         user = User.objects.get(email="user5@test.ac.uk")
@@ -23,7 +31,7 @@ class LGDGetCurationDraftEndpoint(TestCase):
         access_token = str(refresh.access_token)
 
         # Authenticate by setting cookie on the test client
-        self.client.cookies[settings.SIMPLE_JWT['AUTH_COOKIE']] = access_token
+        self.client.cookies[settings.SIMPLE_JWT["AUTH_COOKIE"]] = access_token
 
         response = self.client.get(self.url_get_curation)
         self.assertEqual(response.status_code, 200)
@@ -32,7 +40,7 @@ class LGDGetCurationDraftEndpoint(TestCase):
 
     def test_get_curation_unauthorised_access(self):
         """
-            Test call to get curation draft endpoint without authentication
+        Test call to get curation draft endpoint without authentication
         """
 
         response = self.client.get(self.url_get_curation)
--- gene2phenotype_project/gene2phenotype_app/tests/curation/test_list_curation.py
+++ gene2phenotype_project/gene2phenotype_app/tests/curation/test_list_curation.py
@@ -4,18 +4,24 @@ from django.conf import settings
 from rest_framework_simplejwt.tokens import RefreshToken
 from gene2phenotype_app.models import User
 
+
 class LGDListCurationDraftsEndpoint(TestCase):
     """
-        Test endpoint to list curation drafts
+    Test endpoint to list curation drafts
     """
-    fixtures = ["gene2phenotype_app/fixtures/g2p_stable_id.json", "gene2phenotype_app/fixtures/user_panels.json", "gene2phenotype_app/fixtures/curation_data.json"]
+
+    fixtures = [
+        "gene2phenotype_app/fixtures/g2p_stable_id.json",
+        "gene2phenotype_app/fixtures/user_panels.json",
+        "gene2phenotype_app/fixtures/curation_data.json",
+    ]
 
     def setUp(self):
         self.url_list_curation = reverse("list_curation_entries")
 
     def test_list_curation_success(self):
         """
-            Test successful call to list curation drafts endpoint
+        Test successful call to list curation drafts endpoint
         """
         # Login
         user = User.objects.get(email="user5@test.ac.uk")
@@ -23,7 +29,7 @@ class LGDListCurationDraftsEndpoint(TestCase):
         access_token = str(refresh.access_token)
 
         # Authenticate by setting cookie on the test client
-        self.client.cookies[settings.SIMPLE_JWT['AUTH_COOKIE']] = access_token
+        self.client.cookies[settings.SIMPLE_JWT["AUTH_COOKIE"]] = access_token
 
         response = self.client.get(self.url_list_curation)
         self.assertEqual(response.status_code, 200)
@@ -32,7 +38,7 @@ class LGDListCurationDraftsEndpoint(TestCase):
 
     def test_list_curation_unauthorised_access(self):
         """
-            Test call to list curation drafts endpoint without authentication
+        Test call to list curation drafts endpoint without authentication
         """
 
         response = self.client.get(self.url_list_curation)
