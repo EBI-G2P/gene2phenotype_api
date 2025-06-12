ea35e6484729ba33ddd1a1ce2dda05f24634f93a
Ola Austine <olaaustine@ebi.ac.uk>
2025-06-12 15:59:40 +0100

    Adding the documentation

--- gene2phenotype_project/gene2phenotype_app/serializers/gencc_submission.py
+++ gene2phenotype_project/gene2phenotype_app/serializers/gencc_submission.py
@@ -1,24 +1,48 @@
 from rest_framework import serializers
 from ..models import GenCCSubmission, G2PStableID, LocusGenotypeDisease
 from django.db.models import OuterRef, Exists, Subquery, DateField, ExpressionWrapper, F
+from typing import Any
+from django.db.models.query import QuerySet
 
 
 
 class GenCCSubmissionSerializer(serializers.ModelSerializer):
-    def create(self, validated_data):
+    """Serializer for the GenCCSubmission
+    """    
+    def create(self, validated_data: dict[str, Any]) -> GenCCSubmission:
+        """Create for the GenCC submission
+
+        Args:
+            validated_data (dict[str, Any]): Validated data dictionary
+
+        Returns:
+            GenCCSubmission: A created GenCC submission object
+        """        
         return GenCCSubmission.objects.create(**validated_data)
 
     @staticmethod
-    def fetch_list_of_unsubmitted_stable_id():
+    def fetch_list_of_unsubmitted_stable_id() -> QuerySet:
+        """Fetch List of unsubmitted stable id from the G2PStableID by comparing whats in the GenCCSubmission table
+
+        Returns:
+            QuerySet: A queryset
+        """           
         return G2PStableID.objects.annotate(
             has_submission=Exists(
                 GenCCSubmission.objects.filter(g2p_stable_id=OuterRef('id'))
             )
         ).filter(has_submission=False, is_live=1)
 
+  
     
     @staticmethod
-    def fetch_stable_ids_with_later_review_date():
+    def fetch_stable_ids_with_later_review_date() -> QuerySet:
+        """Fetches Stable ID that has been updated since the last GenCC submission
+
+        Returns:
+            QuerySet: A queryset
+        """        
+        
         return G2PStableID.objects.filter(
             Exists(
                 LocusGenotypeDisease.objects.filter(
--- gene2phenotype_project/gene2phenotype_app/views/gencc_submission.py
+++ gene2phenotype_project/gene2phenotype_app/views/gencc_submission.py
@@ -5,13 +5,30 @@ from rest_framework import status
 from gene2phenotype_app.serializers import GenCCSubmissionSerializer, G2PStableIDSerializer
 
 class GenCCSubmissionView(APIView):
-    def get(self, request):
+    """GenCCSubmissionView 
+    """    
+    def get(self, request) -> Response:
+        """Gets the unsubmitted stable ids
+
+        Returns:
+            Response: Response containing the status and the serializer.data
+        """        
         unused_ids = GenCCSubmissionSerializer.fetch_list_of_unsubmitted_stable_id()
         serializer = G2PStableIDSerializer(unused_ids, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
 
 class StableIDsWithLaterReviewDateView(APIView):
-    def get(self, request):
+    """StableIDs with laterReview date
+    """    
+    def get(self, request)-> Response:
+        """Gets the Stable IDs that were reviewed later
+
+        Returns:
+            Response: Response object containing the 
+             stable_ids as a list 
+             the count of stable_ids that fit this criteria 
+             status
+        """        
         stable_ids = GenCCSubmissionSerializer.fetch_stable_ids_with_later_review_date()
         return Response({"stable_ids": list(stable_ids), "count": len(list(stable_ids))}, status=status.HTTP_200_OK)
