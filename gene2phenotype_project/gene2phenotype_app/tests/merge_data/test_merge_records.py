585b730d3800cb4b34143eacf1511fafdc10901d
dglemos <dlemos@ebi.ac.uk>
2025-06-17 16:27:17 +0100

    Format file

--- gene2phenotype_project/gene2phenotype_app/tests/merge_data/test_merge_records.py
+++ gene2phenotype_project/gene2phenotype_app/tests/merge_data/test_merge_records.py
@@ -4,20 +4,31 @@ from django.conf import settings
 from rest_framework_simplejwt.tokens import RefreshToken
 from gene2phenotype_app.models import User, LGDPublication
 
+
 class LGDEditPublicationsEndpoint(TestCase):
     """
     Test endpoint to merge records
     """
-    fixtures = ["gene2phenotype_app/fixtures/attribs.json", "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
-                "gene2phenotype_app/fixtures/disease.json", "gene2phenotype_app/fixtures/g2p_stable_id.json",
-                "gene2phenotype_app/fixtures/g2p_stable_id.json", "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
-                "gene2phenotype_app/fixtures/lgd_mechanism_evidence.json", "gene2phenotype_app/fixtures/lgd_mechanism_synopsis.json",
-                "gene2phenotype_app/fixtures/lgd_panel.json", "gene2phenotype_app/fixtures/locus_genotype_disease.json",
-                "gene2phenotype_app/fixtures/locus.json", "gene2phenotype_app/fixtures/publication.json",
-                "gene2phenotype_app/fixtures/sequence.json", "gene2phenotype_app/fixtures/user_panels.json",
-                "gene2phenotype_app/fixtures/ontology_term.json", "gene2phenotype_app/fixtures/source.json",
-                "gene2phenotype_app/fixtures/lgd_publication.json"
-                ]
+
+    fixtures = [
+        "gene2phenotype_app/fixtures/attribs.json",
+        "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
+        "gene2phenotype_app/fixtures/disease.json",
+        "gene2phenotype_app/fixtures/g2p_stable_id.json",
+        "gene2phenotype_app/fixtures/g2p_stable_id.json",
+        "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
+        "gene2phenotype_app/fixtures/lgd_mechanism_evidence.json",
+        "gene2phenotype_app/fixtures/lgd_mechanism_synopsis.json",
+        "gene2phenotype_app/fixtures/lgd_panel.json",
+        "gene2phenotype_app/fixtures/locus_genotype_disease.json",
+        "gene2phenotype_app/fixtures/locus.json",
+        "gene2phenotype_app/fixtures/publication.json",
+        "gene2phenotype_app/fixtures/sequence.json",
+        "gene2phenotype_app/fixtures/user_panels.json",
+        "gene2phenotype_app/fixtures/ontology_term.json",
+        "gene2phenotype_app/fixtures/source.json",
+        "gene2phenotype_app/fixtures/lgd_publication.json",
+    ]
 
     def test_lgd_detail(self):
         """
@@ -34,15 +45,16 @@ class LGDEditPublicationsEndpoint(TestCase):
         access_token = str(refresh.access_token)
 
         # Authenticate by setting cookie on the test client
-        self.client.cookies[settings.SIMPLE_JWT['AUTH_COOKIE']] = access_token
+        self.client.cookies[settings.SIMPLE_JWT["AUTH_COOKIE"]] = access_token
 
-        response = self.client.post(url_merge, records_to_merge, content_type="application/json")
+        response = self.client.post(
+            url_merge, records_to_merge, content_type="application/json"
+        )
         self.assertEqual(response.status_code, 400)
 
         response_data = response.json()
         expected_error = [
-            {'error': 'Invalid G2P record G2P11113'},
-            {'error': 'Invalid G2P record G2P11111'}
+            {"error": "Invalid G2P record G2P11113"},
+            {"error": "Invalid G2P record G2P11111"},
         ]
         self.assertEqual(response_data["error"], expected_error)
-
