fbb27b924106b1977e919f1c8073b66f360af4c2
dglemos <dlemos@ebi.ac.uk>
2025-07-07 18:10:31 +0100

    Update data checks after model update

--- gene2phenotype_project/gene2phenotype_app/management/commands/datachecks/PublicationFamilies.py
+++ gene2phenotype_project/gene2phenotype_app/management/commands/datachecks/PublicationFamilies.py
@@ -1,19 +1,21 @@
 from django.core.checks import Error
-from gene2phenotype_app.models import PublicationFamilies
+from gene2phenotype_app.models import LGDPublication
 from django.db.models import F
 
 
 def check_model_constraints():
     errors = []
 
-    pub_families_check = PublicationFamilies.objects.filter(families__gt=F("affected_individuals"))
+    pub_families_check = LGDPublication.objects.filter(
+        number_of_families__gt=F("affected_individuals"), is_deleted=0
+    )
     for obj in pub_families_check:
         errors.append(
             Error(
-                f"{obj.publication_id} has number of families greater than the number of affected individuals",
+                f"Publication ID {obj.publication_id} has number of families greater than the number of affected individuals",
                 hint="Number of families can not be greater than affected individuals",
                 id="gene2phenotype_app.E001",
             )
-            )
+        )
 
     return errors
