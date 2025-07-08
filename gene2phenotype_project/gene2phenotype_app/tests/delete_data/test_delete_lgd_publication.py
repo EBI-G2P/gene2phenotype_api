80d9b86ab0517e784382d6310a3e4d1fffdc90d7
dglemos <dlemos@ebi.ac.uk>
2025-07-08 14:31:40 +0100

    Add more tests

--- gene2phenotype_project/gene2phenotype_app/tests/delete_data/test_delete_lgd_publication.py
+++ gene2phenotype_project/gene2phenotype_app/tests/delete_data/test_delete_lgd_publication.py
@@ -35,7 +35,6 @@ class LGDDeletePublication(TestCase):
         "gene2phenotype_app/fixtures/lgd_variant_type_comment.json",
         "gene2phenotype_app/fixtures/lgd_variant_type_description.json",
         "gene2phenotype_app/fixtures/lgd_variant_consequence.json",
-        "gene2phenotype_app/fixtures/lgd_cross_cutting_modifier.json",
     ]
 
     def setUp(self):
@@ -59,6 +58,32 @@ class LGDDeletePublication(TestCase):
             self.url_delete, to_delete, content_type="application/json"
         )
         self.assertEqual(response.status_code, 403)
+
+    def test_forbidden_delete(self):
+        """
+        Test deleting the publication for record with only one publication
+        """
+        url_no_delete = reverse("lgd_publication", kwargs={"stable_id": "G2P00001"})
+        to_delete = {"pmid": 3897232}
+
+        # Login
+        user = User.objects.get(email="john@test.ac.uk")
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+
+        # Authenticate by setting cookie on the test client
+        self.client.cookies[settings.SIMPLE_JWT["AUTH_COOKIE"]] = access_token
+
+        response = self.client.patch(
+            url_no_delete, to_delete, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+
+        response_data = response.json()
+        self.assertEqual(
+            response_data["error"],
+            "Could not delete PMID '3897232' for ID 'G2P00001'",
+        )
 
     def test_invalid_delete(self):
         """
