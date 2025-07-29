19320bebd679da9c2c10950ceadf722b1069f699
dglemos <dlemos@ebi.ac.uk>
2025-07-29 17:10:13 +0100

    Add more tests

--- gene2phenotype_project/gene2phenotype_app/tests/views/test_disease.py
+++ gene2phenotype_project/gene2phenotype_app/tests/views/test_disease.py
@@ -1,22 +1,26 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 class DiseaseEndpointTests(TestCase):
     """
-        Test the disease endpoint: DiseaseDetail
+    Test the disease endpoint: DiseaseDetail
     """
+
     fixtures = [
         "gene2phenotype_app/fixtures/disease.json",
         "gene2phenotype_app/fixtures/attribs.json",
         "gene2phenotype_app/fixtures/source.json",
-        "gene2phenotype_app/fixtures/ontology_term.json"
+        "gene2phenotype_app/fixtures/ontology_term.json",
     ]
 
     def test_get_disease(self):
         """
-            Test the response of the disease detail endpoint
+        Test the response of the disease detail endpoint
         """
-        self.url_disease = reverse("disease_details", kwargs={"id": "Congenital ichthyosis type 1"})
+        self.url_disease = reverse(
+            "disease_details", kwargs={"id": "Congenital ichthyosis type 1"}
+        )
 
         response = self.client.get(self.url_disease)
 
@@ -25,30 +29,39 @@ class DiseaseEndpointTests(TestCase):
         self.assertEqual(response.data["last_updated"], None)
         self.assertEqual(response.data["synonyms"], [])
 
-        expected_data = [{
-            "accession": "MONDO:0009441",
-            "term": "MONDO:0009441",
-            "description": "Any autosomal recessive congenital ichthyosis in which the cause of the disease is a mutation in the TGM1 gene.",
-            "source": "Mondo"
-        }]
+        expected_data = [
+            {
+                "accession": "MONDO:0009441",
+                "term": "MONDO:0009441",
+                "description": "Any autosomal recessive congenital ichthyosis in which the cause of the disease is a mutation in the TGM1 gene.",
+                "source": "Mondo",
+            }
+        ]
         self.assertEqual(list(response.data["ontology_terms"]), expected_data)
 
     def test_not_found(self):
         """
-            Test the response of the disease detail endpoint
-            when disease is not found
+        Test the response of the disease detail endpoint
+        when disease is not found
         """
-        self.url_disease = reverse("disease_details", kwargs={"id": "CACNA1F-related Aland Island"})
+        self.url_disease = reverse(
+            "disease_details", kwargs={"id": "CACNA1F-related Aland Island"}
+        )
 
         response = self.client.get(self.url_disease)
 
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(response.data["error"], "No matching Disease found for: CACNA1F-related Aland Island")
+        self.assertEqual(
+            response.data["error"],
+            "No matching Disease found for: CACNA1F-related Aland Island",
+        )
+
 
 class DiseaseSummaryTests(TestCase):
     """
-        Test the disease endpoint: DiseaseSummary
+    Test the disease endpoint: DiseaseSummary
     """
+
     fixtures = [
         "gene2phenotype_app/fixtures/disease.json",
         "gene2phenotype_app/fixtures/attribs.json",
@@ -60,19 +73,23 @@ class DiseaseSummaryTests(TestCase):
         "gene2phenotype_app/fixtures/locus.json",
         "gene2phenotype_app/fixtures/sequence.json",
         "gene2phenotype_app/fixtures/ontology_term.json",
-        "gene2phenotype_app/fixtures/source.json"
+        "gene2phenotype_app/fixtures/source.json",
     ]
 
     def test_get_disease(self):
         """
-            Test the response of the disease summary endpoint
+        Test the response of the disease summary endpoint
         """
-        self.url_disease = reverse("disease_summary", kwargs={"id": "CEP290-related JOUBERT SYNDROME TYPE 5"})
+        self.url_disease = reverse(
+            "disease_summary", kwargs={"id": "CEP290-related JOUBERT SYNDROME TYPE 5"}
+        )
 
         response = self.client.get(self.url_disease)
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["disease"], "CEP290-related JOUBERT SYNDROME TYPE 5")
+        self.assertEqual(
+            response.data["disease"], "CEP290-related JOUBERT SYNDROME TYPE 5"
+        )
 
         expected_data = [
             {
@@ -83,19 +100,24 @@ class DiseaseSummaryTests(TestCase):
                 "variant_consequence": [None],
                 "variant_type": [],
                 "molecular_mechanism": "loss of function",
-                "stable_id": "G2P00001"
+                "stable_id": "G2P00001",
             }
         ]
         self.assertEqual(list(response.data["records_summary"]), expected_data)
 
     def test_not_found(self):
         """
-            Test the response of the disease summary endpoint
-            when disease is not found
+        Test the response of the disease summary endpoint
+        when disease is not found
         """
-        self.url_disease = reverse("disease_summary", kwargs={"id": "CACNA1F-related Aland Island"})
+        self.url_disease = reverse(
+            "disease_summary", kwargs={"id": "CACNA1F-related Aland Island"}
+        )
 
         response = self.client.get(self.url_disease)
 
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(response.data["error"], "No matching Disease found for: CACNA1F-related Aland Island")
+        self.assertEqual(
+            response.data["error"],
+            "No matching Disease found for: CACNA1F-related Aland Island",
+        )
--- gene2phenotype_project/gene2phenotype_app/tests/views/test_gene.py
+++ gene2phenotype_project/gene2phenotype_app/tests/views/test_gene.py
@@ -1,19 +1,25 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 class GeneEndpointTests(TestCase):
     """
-        Test the gene endpoint: LocusGene
+    Test the gene endpoint: LocusGene
     """
-    fixtures = ["gene2phenotype_app/fixtures/locus.json", "gene2phenotype_app/fixtures/attribs.json",
-                "gene2phenotype_app/fixtures/source.json", "gene2phenotype_app/fixtures/sequence.json"]
+
+    fixtures = [
+        "gene2phenotype_app/fixtures/locus.json",
+        "gene2phenotype_app/fixtures/attribs.json",
+        "gene2phenotype_app/fixtures/source.json",
+        "gene2phenotype_app/fixtures/sequence.json",
+    ]
 
     def setUp(self):
         self.url_gene = reverse("locus_gene", kwargs={"name": "CEP290"})
 
     def test_get_gene(self):
         """
-            Test the response of the gene endpoint
+        Test the response of the gene endpoint
         """
         response = self.client.get(self.url_gene)
 
@@ -23,36 +29,44 @@ class GeneEndpointTests(TestCase):
         self.assertEqual(response.data["start"], 88049016)
         self.assertEqual(response.data["end"], 88142099)
 
-        expected_data_ids = {
-            "HGNC": "HGNC:29021",
-            "Ensembl": "ENSG00000198707"
-        }
+        expected_data_ids = {"HGNC": "HGNC:29021", "Ensembl": "ENSG00000198707"}
         self.assertEqual(response.data["ids"], expected_data_ids)
 
         expected_data_synonyms = ["BBS14", "CT87"]
         self.assertEqual(list(response.data["synonyms"]), expected_data_synonyms)
 
+
 class GeneSummaryEndpointTests(TestCase):
     """
-        Test the gene summary endpoint: LocusGeneSummary
+    Test the gene summary endpoint: LocusGeneSummary
     """
-    fixtures = ["gene2phenotype_app/fixtures/attribs.json", "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
-                "gene2phenotype_app/fixtures/disease.json", "gene2phenotype_app/fixtures/g2p_stable_id.json",
-                "gene2phenotype_app/fixtures/g2p_stable_id.json", "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
-                "gene2phenotype_app/fixtures/lgd_mechanism_evidence.json", "gene2phenotype_app/fixtures/lgd_mechanism_synopsis.json",
-                "gene2phenotype_app/fixtures/lgd_panel.json", "gene2phenotype_app/fixtures/locus_genotype_disease.json",
-                "gene2phenotype_app/fixtures/locus.json", "gene2phenotype_app/fixtures/publication.json",
-                "gene2phenotype_app/fixtures/sequence.json", "gene2phenotype_app/fixtures/user_panels.json",
-                "gene2phenotype_app/fixtures/ontology_term.json", "gene2phenotype_app/fixtures/source.json",
-                "gene2phenotype_app/fixtures/lgd_publication.json"
-                ]
+
+    fixtures = [
+        "gene2phenotype_app/fixtures/attribs.json",
+        "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
+        "gene2phenotype_app/fixtures/disease.json",
+        "gene2phenotype_app/fixtures/g2p_stable_id.json",
+        "gene2phenotype_app/fixtures/g2p_stable_id.json",
+        "gene2phenotype_app/fixtures/cv_molecular_mechanism.json",
+        "gene2phenotype_app/fixtures/lgd_mechanism_evidence.json",
+        "gene2phenotype_app/fixtures/lgd_mechanism_synopsis.json",
+        "gene2phenotype_app/fixtures/lgd_panel.json",
+        "gene2phenotype_app/fixtures/locus_genotype_disease.json",
+        "gene2phenotype_app/fixtures/locus.json",
+        "gene2phenotype_app/fixtures/publication.json",
+        "gene2phenotype_app/fixtures/sequence.json",
+        "gene2phenotype_app/fixtures/user_panels.json",
+        "gene2phenotype_app/fixtures/ontology_term.json",
+        "gene2phenotype_app/fixtures/source.json",
+        "gene2phenotype_app/fixtures/lgd_publication.json",
+    ]
 
     def setUp(self):
         self.url_gene_summary = reverse("locus_gene_summary", kwargs={"name": "CEP290"})
 
     def test_get_summary(self):
         """
-            Test the response of the gene summary endpoint
+        Test the response of the gene summary endpoint
         """
         response = self.client.get(self.url_gene_summary)
 
@@ -60,60 +74,78 @@ class GeneSummaryEndpointTests(TestCase):
 
         expected_data = [
             {
-                'disease': 'CEP290-related JOUBERT SYNDROME TYPE 5',
-                'genotype': 'biallelic_autosomal',
-                'confidence': 'definitive',
-                'panels': ['DD', 'Eye'],
-                'variant_consequence': [None],
-                'variant_type': [],
-                'molecular_mechanism': 'loss of function',
-                'last_updated': '2017-04-24',
-                'stable_id': 'G2P00001'
+                "disease": "CEP290-related JOUBERT SYNDROME TYPE 5",
+                "genotype": "biallelic_autosomal",
+                "confidence": "definitive",
+                "panels": ["DD", "Eye"],
+                "variant_consequence": [None],
+                "variant_type": [],
+                "molecular_mechanism": "loss of function",
+                "last_updated": "2017-04-24",
+                "stable_id": "G2P00001",
             }
         ]
         self.assertEqual(list(response.data["records_summary"]), expected_data)
 
+
 class GeneFunctionEndpointTests(TestCase):
     """
-        Test the gene function endpoint: GeneFunction
+    Test the gene function endpoint: GeneFunction
     """
-    fixtures = ["gene2phenotype_app/fixtures/locus.json", "gene2phenotype_app/fixtures/attribs.json",
-                "gene2phenotype_app/fixtures/source.json", "gene2phenotype_app/fixtures/sequence.json",
-                "gene2phenotype_app/fixtures/uniprot_annotation.json", "gene2phenotype_app/fixtures/gene_stats.json"]
+
+    fixtures = [
+        "gene2phenotype_app/fixtures/locus.json",
+        "gene2phenotype_app/fixtures/attribs.json",
+        "gene2phenotype_app/fixtures/source.json",
+        "gene2phenotype_app/fixtures/sequence.json",
+        "gene2phenotype_app/fixtures/uniprot_annotation.json",
+        "gene2phenotype_app/fixtures/gene_stats.json",
+    ]
 
     def setUp(self):
-        self.url_gene_function = reverse("locus_gene_function", kwargs={"name": "CEP290"})
+        self.url_gene_function = reverse(
+            "locus_gene_function", kwargs={"name": "CEP290"}
+        )
 
     def test_get_function(self):
         """
-            Test the response of the gene function endpoint
+        Test the response of the gene function endpoint
         """
         response = self.client.get(self.url_gene_function)
 
         self.assertEqual(response.status_code, 200)
 
         expected_data_function = {
-            'protein_function': 'Involved in early and late steps in cilia formation. Its association with CCP110 is required for inhibition of primary cilia formation by CCP110 (PubMed:18694559).',
-            'uniprot_accession': 'O15078'
+            "protein_function": "Involved in early and late steps in cilia formation. Its association with CCP110 is required for inhibition of primary cilia formation by CCP110 (PubMed:18694559).",
+            "uniprot_accession": "O15078",
         }
         self.assertEqual(response.data["function"], expected_data_function)
 
-        self.assertEqual(response.data["gene_stats"], {'gain_of_function_mp': 0.637})
+        self.assertEqual(response.data["gene_stats"], {"gain_of_function_mp": 0.637})
+
 
 class GeneDiseaseEndpointTests(TestCase):
     """
-        Test the gene disease endpoint: GeneDiseaseView
+    Test the gene disease endpoint: GeneDiseaseView
     """
-    fixtures = ["gene2phenotype_app/fixtures/locus.json", "gene2phenotype_app/fixtures/attribs.json",
-                "gene2phenotype_app/fixtures/source.json", "gene2phenotype_app/fixtures/sequence.json",
-                "gene2phenotype_app/fixtures/gene_disease.json"]
+
+    fixtures = [
+        "gene2phenotype_app/fixtures/locus.json",
+        "gene2phenotype_app/fixtures/attribs.json",
+        "gene2phenotype_app/fixtures/source.json",
+        "gene2phenotype_app/fixtures/sequence.json",
+        "gene2phenotype_app/fixtures/gene_disease.json",
+    ]
 
     def setUp(self):
         self.url_gene = reverse("locus_gene_disease", kwargs={"name": "CEP290"})
+        self.url_gene_synonym = reverse("locus_gene_disease", kwargs={"name": "BBS14"})
+        self.url_invalid_gene = reverse("locus_gene_disease", kwargs={"name": "BBBS14"})
+        self.url_not_found = reverse("locus_gene_disease", kwargs={"name": "GS2"})
 
     def test_get_gene(self):
         """
-            Test the response of the gene disease endpoint
+        Test the response of the gene disease endpoint
         """
         response = self.client.get(self.url_gene)
 
@@ -121,9 +153,88 @@ class GeneDiseaseEndpointTests(TestCase):
         self.assertEqual(response.data["count"], 4)
 
         expected_data_function = [
-            {'original_disease_name': 'JOUBERT SYNDROME 5', 'disease_name': 'joubert syndrome', 'identifier': '610188', 'source': 'OMIM'},
-            {'original_disease_name': 'SENIOR-LOKEN SYNDROME 6', 'disease_name': 'senior-loken syndrome', 'identifier': '610189', 'source': 'OMIM'},
-            {'original_disease_name': 'Joubert syndrome 5', 'disease_name': 'joubert syndrome', 'identifier': 'MONDO:0012432', 'source': 'Mondo'},
-            {'original_disease_name': 'Senior-Loken syndrome 6', 'disease_name': 'senior-loken syndrome', 'identifier': 'MONDO:0012433', 'source': 'Mondo'}
+            {
+                "original_disease_name": "JOUBERT SYNDROME 5",
+                "disease_name": "joubert syndrome",
+                "identifier": "610188",
+                "source": "OMIM",
+            },
+            {
+                "original_disease_name": "SENIOR-LOKEN SYNDROME 6",
+                "disease_name": "senior-loken syndrome",
+                "identifier": "610189",
+                "source": "OMIM",
+            },
+            {
+                "original_disease_name": "Joubert syndrome 5",
+                "disease_name": "joubert syndrome",
+                "identifier": "MONDO:0012432",
+                "source": "Mondo",
+            },
+            {
+                "original_disease_name": "Senior-Loken syndrome 6",
+                "disease_name": "senior-loken syndrome",
+                "identifier": "MONDO:0012433",
+                "source": "Mondo",
+            },
         ]
         self.assertEqual(response.data["results"], expected_data_function)
+
+    def test_get_gene_synonym(self):
+        """
+        Test the response of the gene disease endpoint when searching the gene synonym.
+        """
+        response = self.client.get(self.url_gene_synonym)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["count"], 4)
+
+        expected_data_function = [
+            {
+                "original_disease_name": "JOUBERT SYNDROME 5",
+                "disease_name": "joubert syndrome",
+                "identifier": "610188",
+                "source": "OMIM",
+            },
+            {
+                "original_disease_name": "SENIOR-LOKEN SYNDROME 6",
+                "disease_name": "senior-loken syndrome",
+                "identifier": "610189",
+                "source": "OMIM",
+            },
+            {
+                "original_disease_name": "Joubert syndrome 5",
+                "disease_name": "joubert syndrome",
+                "identifier": "MONDO:0012432",
+                "source": "Mondo",
+            },
+            {
+                "original_disease_name": "Senior-Loken syndrome 6",
+                "disease_name": "senior-loken syndrome",
+                "identifier": "MONDO:0012433",
+                "source": "Mondo",
+            },
+        ]
+        self.assertEqual(response.data["results"], expected_data_function)
+
+    def test_get_invalid_gene(self):
+        """
+        Test the response of the gene disease endpoint when searching for an invalid gene.
+        """
+        response = self.client.get(self.url_invalid_gene)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data["error"], "No matching Gene found for: BBBS14")
+
+    def test_not_found(self):
+        """
+        Test the response of the gene disease endpoint when no association found.
+        """
+        response = self.client.get(self.url_not_found)
+
+        self.assertEqual(response.status_code, 404)
+        print("->", response.data)
+        self.assertEqual(
+            response.data["error"],
+            "No matching Gene-Disease association found for: GS2",
+        )
