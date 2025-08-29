38372de73ab6f7e808d7c3238f2184bfc65bc146
dglemos <dlemos@ebi.ac.uk>
2025-08-29 10:45:00 +0100

    Add checks

--- gene2phenotype_project/gene2phenotype_app/management/commands/load_mined_publications.py
+++ gene2phenotype_project/gene2phenotype_app/management/commands/load_mined_publications.py
@@ -1,10 +1,11 @@
 import csv
 import logging
 import re
+import os.path
 
 from django.core.management.base import BaseCommand, CommandError
 
-from ...utils import get_publication, get_authors, clean_title, get_date_now
+from ...utils import get_publication, clean_title, get_date_now
 
 from gene2phenotype_app.models import (
     Publication,
@@ -18,11 +19,14 @@ from gene2phenotype_app.models import (
 
 """
 Command to load mined publications from a csv file.
-File format is the following:
+The mined publications are going to be saved in tables 'mined_publications' and 'lgd_mined_publications'.
 
+File format is the following:
+PMID\tG2P_IDs
 """
 
 logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -30,7 +34,7 @@ class Command(BaseCommand):
             "--data_file",
             required=True,
             type=str,
-            help="Input file containing the mined publications",
+            help="Input file containing the mined publications (supported format: csv)",
         )
         parser.add_argument(
             "--email",
@@ -43,7 +47,14 @@ class Command(BaseCommand):
         data_file = options["data_file"]
         input_email = options["email"]
 
-        # TODO: check input file headers
+        if not os.path.isfile(data_file):
+            raise CommandError(f"Invalid file {data_file}")
+
+        if not data_file.endswith(".csv"):
+            raise CommandError(f"Unsupported file format {data_file}")
+
+        # Define the mandatory input headers
+        mandatory_headers = ["PMID", "G2P_IDs"]
 
         # Get user info
         try:
@@ -53,6 +64,15 @@ class Command(BaseCommand):
 
         with open(data_file, newline="") as fh_file:
             data_reader = csv.DictReader(fh_file)
+
+            # Check headers
+            if not all(
+                column in data_reader.fieldnames for column in mandatory_headers
+            ):
+                raise CommandError(
+                    f"Missing data. Mandatory fields are: {mandatory_headers}"
+                )
+
             for row in data_reader:
                 pmid = row["PMID"].strip()
                 g2p_ids = row["G2P_IDs"].strip()
@@ -64,11 +84,9 @@ class Command(BaseCommand):
                 list_g2p_ids = g2p_ids.split(";")
                 # to make sure we don't try to insert duplicates
                 final_list_g2p_ids = []
-                
+
                 try:
-                    mined_publication_obj = MinedPublication.objects.get(
-                        pmid=int(pmid)
-                    )
+                    mined_publication_obj = MinedPublication.objects.get(pmid=int(pmid))
                 except MinedPublication.DoesNotExist:
                     response = get_publication(int(pmid))
                     if response["hitCount"] == 0:
@@ -82,40 +100,47 @@ class Command(BaseCommand):
                     mined_publication_obj._history_user = user_obj
                     mined_publication_obj.save()
 
-                # Clean the IDs
+                # Clean the IDs, just in case the ID includes the disease name
                 for g2p_id in list_g2p_ids:
                     new_g2p_id = re.sub(":.*", "", g2p_id)
-                    print(new_g2p_id)
+
                     if new_g2p_id not in final_list_g2p_ids:
                         final_list_g2p_ids.append(new_g2p_id)
 
                         # Get the LocusGenotypeDisease for the G2P ID
                         try:
                             lgd_obj = LocusGenotypeDisease.objects.get(
-                                stable_id__stable_id = new_g2p_id,
-                                is_deleted = 0
+                                stable_id__stable_id=new_g2p_id, is_deleted=0
                             )
                         except LocusGenotypeDisease.DoesNotExist:
                             raise CommandError(f"Invalid G2P ID {new_g2p_id}")
 
-                        # Insert the LGDMinedPublication obj
-                        # Before insertion we need to know if the LGD-publication association already exists
+                        # Check if LGDMinedPublication already exists
                         try:
-                            lgd_publication_obj = LGDPublication.objects.get(
-                                lgd = lgd_obj,
-                                publication__pmid = pmid,
-                                is_deleted = 0
+                            LGDMinedPublication.objects.get(
+                                lgd=lgd_obj, mined_publication=mined_publication_obj
                             )
-                        except LGDPublication.DoesNotExist:
-                            status = "mined"
+                        except LGDMinedPublication.DoesNotExist:
+                            # Insert the LGDMinedPublication obj
+                            # Before insertion we need to know if the LGD-publication association already exists
+                            try:
+                                lgd_publication_obj = LGDPublication.objects.get(
+                                    lgd=lgd_obj, publication__pmid=pmid, is_deleted=0
+                                )
+                            except LGDPublication.DoesNotExist:
+                                status = "mined"
+                            else:
+                                status = "curated"
+
+                            lgd_mined_pub_obj = LGDMinedPublication(
+                                lgd=lgd_obj,
+                                mined_publication=mined_publication_obj,
+                                status=status,
+                                comment=None,
+                            )
+                            lgd_mined_pub_obj._history_user = user_obj
+                            lgd_mined_pub_obj.save()
                         else:
-                            status = "curated"
-                        
-                        lgd_mined_pub_obj = LGDMinedPublication(
-                            lgd = lgd_obj,
-                            mined_publication = mined_publication_obj,
-                            status = status,
-                            comment = None
-                        )
-                        lgd_mined_pub_obj._history_user = user_obj
-                        lgd_mined_pub_obj.save()
+                            logger.warning(
+                                f"{new_g2p_id}-{pmid} already exists. Skipping import."
+                            )
