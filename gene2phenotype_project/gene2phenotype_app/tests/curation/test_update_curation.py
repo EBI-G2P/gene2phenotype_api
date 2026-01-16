aede2e5c25b786a78d88fe6c01a4e8d731689a5b
dglemos <dlemos@ebi.ac.uk>
2026-01-16 10:40:04 +0000

    Add test removed by mistake

--- gene2phenotype_project/gene2phenotype_app/tests/curation/test_update_curation.py
+++ gene2phenotype_project/gene2phenotype_app/tests/curation/test_update_curation.py
@@ -36,6 +36,124 @@ class LGDUpdateCurationEndpoint(TestCase):
             refresh.access_token
         )
 
+    def test_update_curation_success(self):
+        """
+        Test successful call to update curation endpoint
+        """
+        self.login_user()
+
+        # Define the complex data structure
+        curation_to_update = {
+            "json_data": {
+                "allelic_requirement": "biallelic_autosomal",
+                "confidence": "limited",
+                "cross_cutting_modifier": ["potential secondary finding"],
+                "disease": {
+                    "cross_references": [
+                        {
+                            "disease_name": "bardet-biedl syndrome",
+                            "identifier": "615991",
+                            "original_disease_name": "BARDET-BIEDL SYNDROME 14",
+                            "source": "OMIM",
+                        }
+                    ],
+                    "disease_name": "CEP290-related bardet-biedl syndrome",
+                },
+                "locus": "CEP290",
+                "mechanism_evidence": [
+                    {
+                        "description": "updated test comment",
+                        "evidence_types": [
+                            {
+                                "primary_type": "Rescue",
+                                "secondary_type": ["Patient Cells"],
+                            }
+                        ],
+                        "pmid": "1",
+                    }
+                ],
+                "mechanism_synopsis": [
+                    {"name": "destabilising LOF", "support": "inferred"}
+                ],
+                "molecular_mechanism": {
+                    "name": "loss of function",
+                    "support": "evidence",
+                },
+                "panels": ["Developmental disorders"],
+                "phenotypes": [
+                    {
+                        "hpo_terms": [
+                            {
+                                "accession": "HP:0012372",
+                                "term": "Abnormal eye morphology",
+                            }
+                        ],
+                        "pmid": "1",
+                        "summary": "updated test comment",
+                    }
+                ],
+                "private_comment": "updated test comment",
+                "public_comment": "updated test comment",
+                "publications": [
+                    {
+                        "affectedIndividuals": 1,
+                        "ancestries": "updated test",
+                        "authors": "Makar AB, McMartin KE, Palese M, Tephly TR.",
+                        "comment": "updated test comment",
+                        "consanguineous": "no",
+                        "families": 1,
+                        "pmid": "1",
+                        "source": "G2P",
+                        "title": "Formate assay in body fluids: application in methanol poisoning.",
+                        "year": 1975,
+                    }
+                ],
+                "session_name": "test session",
+                "variant_consequences": [
+                    {
+                        "support": "inferred",
+                        "variant_consequence": "altered_gene_product_level",
+                    }
+                ],
+                "variant_descriptions": [
+                    {"description": "updated test description", "publication": "1"}
+                ],
+                "variant_types": [
+                    {
+                        "comment": "updated test comment",
+                        "de_novo": True,
+                        "inherited": True,
+                        "nmd_escape": False,
+                        "primary_type": "protein_changing",
+                        "secondary_type": "missense_variant",
+                        "supporting_papers": ["1"],
+                        "unknown_inheritance": False,
+                    }
+                ],
+            }
+        }
+
+        response = self.client.put(
+            self.url_update_curation,
+            curation_to_update,
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+        response_data = response.json()
+        self.assertEqual(
+            response_data["message"],
+            "Data updated successfully for session name 'test session'",
+        )
+
+        # Check curation_data table
+        curation_entries = CurationData.objects.filter(session_name="test session")
+        self.assertEqual(len(curation_entries), 1)
+
+        # Test history table
+        history_records = CurationData.history.filter(stable_id__stable_id="G2P00004")
+        self.assertEqual(len(history_records), 1)
+
     def test_update_curation_no_permission(self):
         """
         Test call to update curation endpoint with super user who does not have permission to update the curation
