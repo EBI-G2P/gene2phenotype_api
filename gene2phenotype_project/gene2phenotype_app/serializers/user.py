673e06d44cd7c7b7a746a2967c9f25fe0e83c816
dglemos <dlemos@ebi.ac.uk>
2026-02-05 17:40:50 +0000

    Check if user exists before trying to verify email

--- gene2phenotype_project/gene2phenotype_app/serializers/user.py
+++ gene2phenotype_project/gene2phenotype_app/serializers/user.py
@@ -519,9 +519,9 @@ class VerifyEmailSerializer(serializers.ModelSerializer):
             dict: a dictionary containg the user information
         """
         email = self.validated_data.get("email")
-        user = User.objects.get(email=email, is_deleted=0)
+        user = User.objects.filter(email=email, is_deleted=0).first()
 
-        # Verify Email and Return Token and id that will be used to PasswordReset
+        # Verify Email and return token and id that will be used to reset the password
         if user:
             reset_token = PasswordResetTokenGenerator().make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.id))
@@ -535,8 +535,10 @@ class VerifyEmailSerializer(serializers.ModelSerializer):
                 reset_link=reset_link,
                 to_email=user.email,
             )
-
-            return {"id": user.id, "email": user.email, "token": reset_token}
+        # If user not found, return a generic response to avoid user enumeration
+        return {
+            "message": "If an account exists for this email, a reset link has been sent."
+        }
 
     class Meta:
         model = User
